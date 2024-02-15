7c91a4d324ed4edd9fce630f0016d7ee35351ac8
projeta618 <projeta618@gmail.com>
2024-02-14 21:29:20 -0300

    feat: Add initial migration for profiles app
    Add initial migration for the profiles app in order to create the necessary database tables and models.
    
    Changes:
    - Added initial migration file `migrations/0001_initial.py` to create the `MailSended` and `Unsuscribe` models for the profiles app.

--- models.py
+++ models.py
@@ -1,3 +1,95 @@
 from django.db import models
+from kernel.models.base_metadata_model import BaseMetadataModel
 
-# Create your models here.
+class MailSended(BaseMetadataModel):
+    """
+    MailSended model to store the mail sended by the system
+    """
+    mail = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    subject = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    body = models.TextField(null=True,
+        blank=True
+    )
+    status = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    error = models.TextField(null=True,
+        blank=True
+    )
+    error_trace = models.TextField(null=True,
+        blank=True
+    )
+    response = models.TextField(null=True,
+        blank=True
+    )
+    response_trace = models.TextField(null=True,
+        blank=True
+    )
+    response_code = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    response_message = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    response_status = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    response_error = models.TextField(null=True,
+        blank=True
+    )
+    response_error_trace = models.TextField(null=True,
+        blank=True
+    )
+    response_error_code = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    response_error_message = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+    response_error_status = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+class Unsuscribe(BaseMetadataModel):
+    """
+    Unsuscribe model to store the unsuscribe request
+    """
+    profile = models.ForeignKey('profiles.Profile',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    mail = models.EmailField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
+
+    email_key = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True
+    )
