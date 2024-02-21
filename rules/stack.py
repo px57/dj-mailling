0280e14b9e015bb9ef5c4c295ee2c1a5f4d98f77
px57 <david@prepera.io>
2024-02-21 20:19:00 -0300

    feat: Add MESSAGE_SWITCHER to RulesStack initialization
    Why:
    - The MESSAGE_SWITCHER is a new feature that needs to be added to the initialization of RulesStack in order to centralize message processing.
    
    Changes:
    - Imported MESSAGE_SWITCHER module to stack.py
    - Initialized MESSAGE_SWITCHER with the MAILLING_RULESTACK instance.

--- rules/stack.py
+++ rules/stack.py
@@ -1,5 +1,7 @@
 
 
 from kernel.interfaces.stack import RulesStack
+from kernel.message.centralize import MESSAGE_SWITCHER
 
 MAILLING_RULESTACK = RulesStack()
+MESSAGE_SWITCHER.load_stack(MAILLING_RULESTACK)
